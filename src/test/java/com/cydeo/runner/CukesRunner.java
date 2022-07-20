72f531887a7267c6b6042f0de26f3a3bfd61a1cf
Tamara10171994 <toma.burban1994@gmail.com>
2022-07-20 12:18:05 -0400

    avenger prep8

--- src/test/java/com/cydeo/runner/CukesRunner.java
+++ src/test/java/com/cydeo/runner/CukesRunner.java
@@ -6,14 +6,21 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        plugin = {"html:target/cucumber-report.html", //html report
-                "me.jvt.cucumber.report.PrettyReports:target/cucumber",
-                "json:target/cucumber.json"}, // cucumber report
+        plugin = {"json:target/cucumber.json",
+                "html:target/cucumber/report.html",
+                "junit:target/junit/junit-report.xml",
+                "rerun:target/rerun.txt",}, // cucumber report
         features = "src/test/resources/features" ,
         glue = "com/cydeo/steps",
         dryRun = false,
-        tags = "",
-        publish = true
+        tags = ""
+       // publish = true
+
+        /*
+         plugin = {"html:target/cucumber-report.html", //html report
+                "me.jvt.cucumber.report.PrettyReports:target/cucumber",
+                "json:target/cucumber.json"}, // cucumber report
+         */
 )
 public class CukesRunner {
 }
