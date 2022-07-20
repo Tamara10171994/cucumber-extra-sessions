8116172350503663dafbccccf096be3fc6974449
Tamara10171994 <toma.burban1994@gmail.com>
2022-07-20 12:11:41 -0400

    avenger prep7

--- src/test/java/com/cydeo/runner/CukesRunner.java
+++ src/test/java/com/cydeo/runner/CukesRunner.java
@@ -12,7 +12,8 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features" ,
         glue = "com/cydeo/steps",
         dryRun = false,
-        tags = ""
+        tags = "",
+        publish = true
 )
 public class CukesRunner {
 }
